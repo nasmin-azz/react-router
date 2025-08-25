ced33335f29d9f7b99ab52bd1d143660cd62376a
nasmin-azz <nastaran.atlas@gmail.com>
2025-08-26 00:18:43 +0330

    react-router,useParams

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import NavMenue from "./NavMenue";
 import NotFound from "./NotFound";
+import HomePage from "./HomePage";
+import ProductDetail from "./ProductDetail";
+
 
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <Router>
-      <NavMenue/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/ProductDetail/:id" element={<ProductDetail/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
